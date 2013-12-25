8af8fa091b82262774bf1feb45e8e35b2994ff86
ian-qiu <qiuyiyi2006@163.com>
2013-12-25 19:10:33 +0800

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -8,3 +8,34 @@ function sub(a,b)
 }
 add.call(sub,3,1);
 
+function Class1()
+{
+    this.name = "class1";
+    this.showNam = function()
+    {
+        console.log(this.name);
+}
+}
+function Class2()
+{
+    this.name = "class2";
+}
+var c1 = new Class1();
+var c2 = new Class2();
+c1.showNam.call(c2);
+
+function Class1()
+{
+    this.showTxt = function(txt)
+    {
+        console.log(txt);
+    }
+}
+function Class2()
+{
+    Class1.call(this);
+}
+var c2 = new Class2();
+c2.showTxt("cc");
+console.log(c2.prototype.toString());
+
